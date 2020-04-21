1b1fcc87d4b8fc4b6439019ae95036b40e56c264
Nam Pham <puma.puma07@gmail.com>
2020-04-21 16:36:00 +0700

    - Add plivo to provider list - Add send_and_wait for debuging - Choose provider base on carrier

--- lib/sms_center.rb
+++ lib/sms_center.rb
@@ -1,8 +1,9 @@
 class SmsCenter
-  autoload :Infobip, 'sms_center/infobip'
-  autoload :Nexmo, 'sms_center/nexmo'
-  autoload :Twilio, 'sms_center/twilio'
-  autoload :Telerivet, 'sms_center/telerivet'
+  autoload :Infobip,    'sms_center/infobip'
+  autoload :Nexmo,      'sms_center/nexmo'
+  autoload :Twilio,     'sms_center/twilio'
+  autoload :Telerivet,  'sms_center/telerivet'
+  autoload :Plivo,      'sms_center/plivo'
 
   @@platforms = nil
   @@platform_by_country = nil
@@ -37,12 +38,10 @@ class SmsCenter
   end
 
   def initialize(content, to_number, options = {})
-    platform_by_country = SmsCenter.platform_by_country
     country = options[:country]
     platform = options[:platform]
 
-    f_platform = country.present? ? platform_by_country[country.downcase.to_sym] : platform_by_country[:default]
-    f_platform = platform.downcase.capitalize if platform.present?
+    f_platform = platform.present? ? platform.downcase.capitalize : platform_by_country(country, to_number)
 
     from_number = SmsCenter.root_number_by_platform[f_platform.to_sym]
     @request = SmsCenter.const_get(f_platform, false).new(content, to_number, from_number, @@keys)
@@ -50,5 +49,23 @@ class SmsCenter
 
   def send
     HTTPClient.new.post_async(@request.api_url, @request.body, @request.headers)
+  end
+
+  def send_and_wait
+    HTTPClient.new.post(@request.api_url, @request.body, @request.headers)
+  end
+
+  private
+  def platform_by_country(country, to_number)
+    platform_by_country = SmsCenter.platform_by_country
+    platform = platform_by_country[country.downcase.to_sym]
+    if platform.is_a?(Hash)
+      phone = Phonelib.parse(to_number, country.downcase.to_sym)
+      platform_by_carrier = platform[phone.carrier.to_sym]
+      platform = platform_by_carrier.present? ? platform_by_carrier : platform[:default]
+    elsif platform.nil?
+      platform = platform_by_country[:default]
+    end
+    platform.downcase.capitalize
   end
 end
